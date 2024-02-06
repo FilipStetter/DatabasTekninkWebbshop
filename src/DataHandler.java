47b3f93366f5e803f14a1e65edf7ca16299c5c60
Filip <filip.stetter@telia.com>
2024-02-06 14:01:46 +0100

    cleanup

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -1,7 +1,5 @@
-import java.sql.*;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
+
 
 public class DataHandler {
 
--- src/Repository.java
+++ src/Repository.java
@@ -1,10 +1,7 @@
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.*;
-import java.util.HashSet;
 import java.util.Properties;
-import java.util.Scanner;
-import java.util.Set;
+
 
 public class Repository {
 
